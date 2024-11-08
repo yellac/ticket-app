b2f1fd77e9f2a42b27a1f96b211fb567ea2c22e7
Steven Huang <hcy0113.dev@gmail.com>
2024-11-08 17:16:12 +0800

    New Error type for general cases

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -17,9 +18,7 @@ const signupHandler = async (req: Request, res: Response): Promise<void> => {
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
-    console.log("Email in use");
-    res.send({});
-    return;
+    throw new BadRequestError("Email in use");
   }
 
   const user = User.build({ email, password });
