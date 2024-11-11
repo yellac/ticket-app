8710ead6cede79eef7370bfc0664867e8dff1001
Steven Huang <hcy0113.dev@gmail.com>
2024-11-11 11:03:10 +0800

    Sign in logic setup

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,64 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { Password } from "../services/password";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("signin");
-});
+const signinHandler = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (!existingUser) {
+    throw new BadRequestError("Invalid credentials");
+  }
+
+  const passwordMatch = await Password.compare(existingUser.password, password);
+  if (!passwordMatch) {
+    throw new BadRequestError("Invalid credentials");
+  }
+
+  // Generate JWT
+  const userJwt = jwt.sign(
+    {
+      id: existingUser.id,
+      email: existingUser.email,
+    },
+    process.env.JWT_KEY!
+  );
+
+  // Store token in session object via cookie session
+  req.session = {
+    jwt: userJwt,
+  };
+
+  //   if (!req.session) {
+  //     throw new BadRequestError("Session not found");
+  //   }
+  //   const jwtMatch = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+  //   if (!jwtMatch) {
+  //     throw new BadRequestError("Invalid JWT token");
+  //   }
+
+  res.status(200).send("Successfully signed in!");
+};
+
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password")
+      .notEmpty()
+      .trim()
+      .withMessage("Please enter a valid password"),
+  ],
+  validateRequest,
+  signinHandler
+);
 
 export { router as signinRouter };
