6f6cb71597c3a95fb806836bbdf92cc382fea032
Steven Huang <hcy0113.dev@gmail.com>
2024-11-11 14:15:33 +0800

    Current user jwt logic update

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("henlo");
-});
+const currentUserHandler = (req: Request, res: Response): void => {
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
+};
+
+router.get("/api/users/currentuser", currentUserHandler);
 
 export { router as currentUserRouter };
