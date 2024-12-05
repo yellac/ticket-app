7391e3bc22938fceb8457473d6949d3e6e2d628c
Steven Huang <hcy0113.dev@gmail.com>
2024-12-05 11:40:58 +0800

    Common library update

---
+++ common/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 20,
+  green: 10,
+  blue: 10,
+};
+
+console.log(color);
+
+export default color;
