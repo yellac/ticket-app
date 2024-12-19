20030b540bfa5d7e5f16520dc3660b93a28cf9e2
Steven Huang <hcy0113.dev@gmail.com>
2024-12-19 12:07:26 +0800

    NATS connection setup and graceful shutdown

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import { natsWrapper } from "./nats-wrapper";
 import { app } from "./app";
 
 const init = async () => {
@@ -11,6 +11,16 @@ const init = async () => {
   }
 
   try {
+    await natsWrapper.connect("ticketing", "dffdsasdf", "http://nats-srv:4222");
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    // SIGINT: When user presses Ctrl+C to terminate
+    process.on("SIGINT", () => natsWrapper.client.close());
+    // SIGTREM: When other processes or the system itself send the terminate signal
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
