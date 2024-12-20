54535f7dceaa54ca39842ece67c746c8c2ef9bd0
Steven Huang <hcy0113.dev@gmail.com>
2024-12-20 14:21:32 +0800

    Orders service setup

---
+++ orders/src/index.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
+import { app } from "./app";
+
+const init = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    // SIGINT: When user presses Ctrl+C to terminate
+    process.on("SIGINT", () => natsWrapper.client.close());
+    // SIGTREM: When other processes or the system itself send the terminate signal
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!");
+  });
+};
+
+init();
