c1d340b8e04d46510aff833230202a268cdfc8ef
Steven Huang <hcy0113.dev@gmail.com>
2024-12-29 16:22:10 +0800

    Order and Ticket models setup
    Implement mongoose models including the ticket reference in Order model using ref/populate

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
